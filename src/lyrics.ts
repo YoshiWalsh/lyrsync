85631ccbd73fad862dafc53bd6b726fb7e0c050b
Joshua Walsh <josh@ymindustries.com>
2019-10-01 15:14:11 +1000

    Initial move to TypeScript

--- src/lyrics.ts
+++ src/lyrics.ts
@@ -1,3 +1,5 @@
+var onYouTubeIframeAPIReady;
+
 const textMovementDistance = 0.4;
 const textMovementTime = 1;
 const cardTransitionTime = 0.5;
@@ -5,7 +7,7 @@ const separatorTransitionTime = 2;
 
 let initialised = false;
 const ytReady = new Promise((resolve) => {
-    window.onYouTubeIframeAPIReady = () => { resolve(); };
+    onYouTubeIframeAPIReady = () => { resolve(); };
 })
 
 const lerp = (a, b, t) => a*(1-t)+b*t;
@@ -15,7 +17,7 @@ const easeInOutQuad = t => t<.5 ? 2*t*t : -1+(4-2*t)*t;
 function init() {
     const youtubePromise = initialiseYoutubePlayer();
 
-    const lyricsFile = document.querySelector("link.lyricsFile").href;
+    const lyricsFile = (document.querySelector<HTMLAnchorElement>("link.lyricsFile")).href;
 
     fetch(lyricsFile).then(response => {
         if(!response.ok) {
@@ -29,12 +31,12 @@ function init() {
             layoutLyrics();
             youtubePromise.then(player => {
                 initialised = true;
-                const start = document.querySelector(".start");
+                const start = document.querySelector<HTMLDivElement>(".start");
                 start.style.display = "block";
                 start.addEventListener("click", () => {
                     start.style.display = "none";
                     player.playVideo();
-                    document.querySelector(".container").style.opacity = 1;
+                    document.querySelector<HTMLDivElement>(".container").style.opacity = '1';
                 })
             });
         }, err => {
@@ -55,17 +57,16 @@ let currentTime = 0;
 let playbackSpeed = 0;
 let playing = false;
 function initialiseYoutubePlayer() {
-    return new Promise((resolve, reject) => {
+    return new Promise<YT.Player>((resolve) => {
         ytReady.then(() => {
-            const youtubePlayer = document.querySelector(".youtubeContainer .player");
+            const youtubePlayer = document.querySelector<HTMLDivElement>(".youtubeContainer .player");
             const youtubeId = youtubePlayer.dataset.youtubeId;
             const player = new YT.Player(youtubePlayer, {
                 height: '100%',
                 width: '100%',
                 videoId: youtubeId,
                 playerVars: {
-                    modestbranding: 1,
-                    wmode: "opaque"
+                    modestbranding: 1
                 },
                 events: {
                     'onReady': () => {
@@ -80,7 +81,7 @@ function initialiseYoutubePlayer() {
                         playbackSpeed = player.getPlaybackRate();
                         currentTime = player.getCurrentTime();
                         playing = player.getPlayerState() == 1;
-                        if(state == 0) {
+                        if(state.data == 0) {
                             player.seekTo(0, true);
                             player.playVideo();
                         }
@@ -131,7 +132,7 @@ function parseLyrics(lyricsFile) {
                 const tagContents = lyricsFile.slice(i + 1, tagEnd);
                 i = tagEnd;
 
-                timecodeMatches = tagContents.match(timecodeRegex);
+                const timecodeMatches = tagContents.match(timecodeRegex);
                 if(timecodeMatches) {
                     const timecode = parseInt(timecodeMatches[1], 10) * 60 + parseInt(timecodeMatches[2], 10) + parseInt(timecodeMatches[3], 10) / 100;
                     // Start new word
@@ -194,7 +195,7 @@ function parseLyrics(lyricsFile) {
     }
 
     cards.forEach(card => {
-        for(voice in card.voices) {
+        for(let voice in card.voices) {
             card.voices[voice].sort((a, b) => a.timecode - b.timecode);
         }
     });
@@ -205,24 +206,24 @@ function parseLyrics(lyricsFile) {
     };
 }
 
-const cardElms = [];
-const container = document.querySelector(".lyricsContainer");
+const cardElms: Array<HTMLDivElement> = [];
+const container = document.querySelector<HTMLDivElement>(".lyricsContainer");
 function renderLyrics(lyricsAst) {
     lyricsAst.cards.forEach(card => {
         const cardElm = document.createElement("div");
-        cardElm.card = card;
+        cardElm['card'] = card;
         cardElm.classList.add("card");
-        cardElm.style.setProperty("--card-start-time", card.timecode * 1000);
+        cardElm.style.setProperty("--card-start-time", "" + card.timecode * 1000);
         const contentsElm = document.createElement("div");
         contentsElm.classList.add("contents");
         let firstVoice = true;
-        for(voice in card.voices) {
+        for(let voice in card.voices) {
             const voiceElm = document.createElement("div");
             if(!firstVoice) {
                 const separatorElm = document.createElement("div");
                 separatorElm.classList.add("voiceSeparator");
-                separatorElm.previousVoice = contentsElm.lastElementChild;
-                separatorElm.nextVoice = voiceElm;
+                separatorElm['previousVoice'] = contentsElm.lastElementChild;
+                separatorElm['nextVoice'] = voiceElm;
                 contentsElm.appendChild(separatorElm);
             }
             voiceElm.classList.add("voice");
@@ -230,7 +231,7 @@ function renderLyrics(lyricsAst) {
             const words = card.voices[voice];
             words.forEach(word => {
                 const wordElm = document.createElement("span");
-                wordElm.word = word;
+                wordElm['word'] = word;
                 wordElm.classList.add("word");
                 wordElm.innerHTML = word.contents
                     .replace(/&/g, "&amp;")
@@ -252,12 +253,12 @@ function renderLyrics(lyricsAst) {
 }
 
 function layoutLyrics() {
-    const container = document.querySelector(".lyricsContainer");
+    const container = document.querySelector<HTMLDivElement>(".lyricsContainer");
 
     // It's important to ensure that all voices have the same height, otherwise the separator will move and it will look bad
-    const cardElms = Array.from(container.querySelectorAll(".card"));
+    const cardElms = Array.from(container.querySelectorAll<HTMLDivElement>(".card"));
     cardElms.forEach(cardElm => {
-        const voiceElms = Array.from(cardElm.querySelectorAll(".voice"));
+        const voiceElms = Array.from(cardElm.querySelectorAll<HTMLDivElement>(".voice"));
         voiceElms.forEach(voiceElm => voiceElm.style.height = "auto");
         const voiceHeight = Math.max(...voiceElms.map(voiceElm => voiceElm.getBoundingClientRect().height));
         voiceElms.forEach(voiceElm => voiceElm.style.height = voiceHeight + "px");
@@ -268,15 +269,15 @@ function layoutLyrics() {
     for(let i = 0; i < cardElms.length; i++) {
         const cardElm = cardElms[i];
 
-        const separators = Array.from(cardElm.querySelectorAll(".voiceSeparator"));
+        const separators = Array.from(cardElm.querySelectorAll<HTMLDivElement>(".voiceSeparator"));
 
         separators.forEach((sepElm, index) => {
-            sepElm.previousWidth = previousSeparatorWidths[index];
-            sepElm.intendedWidth = (sepElm.previousVoice.getBoundingClientRect().width + sepElm.nextVoice.getBoundingClientRect().width) / 2;
-            sepElm.style.transform = "scaleX(" + sepElm.intendedWidth + ")"; // This prevents inactive separators from causing layout issues if the browser is resized
+            sepElm['previousWidth'] = previousSeparatorWidths[index];
+            sepElm['intendedWidth'] = (sepElm['previousVoice'].getBoundingClientRect().width + sepElm['nextVoice'].getBoundingClientRect().width) / 2;
+            sepElm.style.transform = "scaleX(" + sepElm['intendedWidth'] + ")"; // This prevents inactive separators from causing layout issues if the browser is resized
         });
 
-        previousSeparatorWidths = separators.map(sepElm => sepElm.intendedWidth);
+        previousSeparatorWidths = separators.map(sepElm => sepElm['intendedWidth']);
     }
 }
 
@@ -295,17 +296,17 @@ function redraw(now) {
     }
     lastDraw = now;
 
-    container.style.setProperty("--current-time", currentTime * 1000);
+    container.style.setProperty("--current-time", "" + currentTime * 1000);
 
     // Set card opacity
-    let activeCards = [];
+    let activeCards: Array<HTMLDivElement> = [];
     for(let i = 0; i < cardElms.length; i++) {
         const cardElm = cardElms[i];
-        if(cardElm.card.timecode > currentTime + cardTransitionTime) {
+        if(cardElm['card'].timecode > currentTime + cardTransitionTime) {
             //cardElm.style.opacity = 0;
             continue;
         }
-        if(cardElm.card.timecode < currentTime) {
+        if(cardElm['card'].timecode < currentTime) {
             //cardElm.style.opacity = 1;
             activeCards = [cardElm];
             continue;
@@ -314,7 +315,7 @@ function redraw(now) {
         if(i > 0) {
             activeCards.push(cardElms[i-1]);
         }
-        const progress = (currentTime - cardElm.card.timecode) / cardTransitionTime + 1;
+        const progress = (currentTime - cardElm['card'].timecode) / cardTransitionTime + 1;
         //cardElm.style.opacity = progress;
     }
 
@@ -322,26 +323,26 @@ function redraw(now) {
     for(let i = 0; i < activeCards.length; i++) {
         const cardElm = activeCards[i];
         const contentsElm = cardElm.children[0];
-        const separators = Array.from(contentsElm.children).filter((_, index) => (index % 2));
+        const separators = <Array<HTMLDivElement>> Array.from(contentsElm.children).filter((_, index) => (index % 2));
         for(let i = 0; i < separators.length; i++) {
             const sepElm = separators[i];
-            if(sepElm.previousWidth) {
-                const progress = Math.max(0, Math.min(1, (currentTime - cardElm.card.timecode) / separatorTransitionTime));
-                sepElm.style.transform = "scaleX(" + lerp(sepElm.previousWidth, sepElm.intendedWidth, easeInOutQuad(progress)) + ")";
+            if(sepElm['previousWidth']) {
+                const progress = Math.max(0, Math.min(1, (currentTime - cardElm['card'].timecode) / separatorTransitionTime));
+                sepElm.style.transform = "scaleX(" + lerp(sepElm['previousWidth'], sepElm['intendedWidth'], easeInOutQuad(progress)) + ")";
             } else {
-                sepElm.style.transform = "scaleX(" + sepElm.intendedWidth + ")";
+                sepElm.style.transform = "scaleX(" + sepElm['intendedWidth'] + ")";
             }
         }
         const voices = Array.from(contentsElm.children).filter((_, index) => !(index % 2));
         for(let i = 0; i < voices.length; i++) {
             const voiceElm = voices[i];
-            const words = Array.from(voiceElm.children);
+            const words = <Array<HTMLDivElement>> Array.from(voiceElm.children);
             const movementDirection = i ? -1 : 1;
             for(let i = 0; i < words.length; i++) {
                 const wordElm = words[i];
-                const word = wordElm.word;
+                const word = wordElm['word'];
                 const progress = Math.max(0, Math.min(1, (currentTime - word.timecode) / textMovementTime));
-                wordElm.style.opacity = progress;
+                wordElm.style.opacity = "" + progress;
                 wordElm.style.transform = "translateY(" + ((1-easeOutQuad(progress)) * movementDirection * textMovementDistance) + "rem)";
             }
         }
@@ -349,4 +350,4 @@ function redraw(now) {
 
     window.requestAnimationFrame(redraw);
 }
-redraw();
+redraw(window.performance.now());
