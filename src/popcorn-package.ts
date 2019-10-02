9e44436125ce88ea15fb8817c88588632ae76fa7
Joshua Walsh <josh@ymindustries.com>
2019-10-02 21:15:15 +1000

    Use Popcorn.js for media handling

---
+++ src/popcorn-package.ts
@@ -0,0 +1,5 @@
+import '/../node_modules/popcorn-js/popcorn.js';
+import '/../node_modules/popcorn-js/modules/player/popcorn.player.js';
+import '/../node_modules/popcorn-js/wrappers/common/popcorn._MediaElementProto.js';
+import '/../node_modules/popcorn-js/wrappers/youtube/popcorn.HTMLYouTubeVideoElement.js';
+import '/../node_modules/popcorn-js/wrappers/vimeo/popcorn.HTMLVimeoVideoElement.js';
