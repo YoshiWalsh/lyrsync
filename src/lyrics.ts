a9877a7ab46f3bf99ca3a81a3ce32df7dec23d8f
Joshua Walsh <josh@ymindustries.com>
2019-10-07 00:07:34 +1100

    Fix a few issues with Du Hast
    Now we also respect the order the author provided the cards in, it's up to them to make sure that order makes sense

--- src/lyrics.ts
+++ src/lyrics.ts
@@ -428,8 +428,6 @@ function parseLyrics(lyricsFile): AST {
         cards.push(currentCard);
     }
 
-    cards.sort((a, b) => a.timecode - b.timecode);
-
     for(let cueName in cues) {
         const currentCues = cues[cueName];
 
